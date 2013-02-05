4b4fd4fcbdfab4ca660767021d4095c44fede773
root <root@debian.(none)>
2013-02-05 12:12:50 +0100

    amelioration de toolchain

--- home/garel/travail/docInterne/toolchain/import.sql
+++ home/garel/travail/docInterne/toolchain/import.sql
@@ -1,4 +1,4 @@
-﻿drop table point;
+﻿-- creation d'une table
 create table point (
     matricule varchar,
     x float,
@@ -7,83 +7,6 @@ create table point (
     code integer);
 -- import des points à partir d'un fichier csv
 copy point
-  from './geodata/vecteur/test/point2.csv'
-  delimiters ';'
+  from '/home/garel/geodata/vecteur/test/point2.csv'
+  delimiters ','
   CSV;
-select addgeometrycolumn('point', 'the_geom',2154,'POINT',3);
-select * from point;
-insert into point (
-    matricule,
-    x,
-    y,
-    z,
-    the_geom)
-  values (
-    1,
-    6898743.87343004,
-    2467468.65469455,
-    0,
-    ST_GeomFromEWKT(
-      'SRID=2154;
-      POINT(
-         6898743.87343004
-         2467468.65469455
-         0)'
-      )
-    );
-
-insert into point (
-    matricule,
-    x,
-    y,
-    z,
-    the_geom)
-  values (
-    2,
-    6898743.87343004,
-    2467468.65469455,
-    0,
-    st_setsrid(
-      st_makepoint(
-         6898743.87343004,
-         2467468.65469455,
-         0),
-      2154)
-    );
-select x,y,st_asewkt(the_geom) from point;
-select
-  x,
-  y,
-  st_asewkt(the_geom), 
-  st_asewkt(st_transform(the_geom, 2154))
-from point;
-
-insert into point (
-    matricule,
-    x,
-    y,
-    z,
-    the_geom)
-  values (
-    3,
-    6898743.87343004,
-    2467468.65469455,
-    0,
-    null
-    );
-
-update point set the_geom=
-    st_setsrid(
-      st_makepoint(
-         6898743.87343004,
-         2467468.65469455,
-         0),
-      2154)
-    where matricule=3;
--- verification
-select
-  x,
-  y,
-  st_asewkt(the_geom), 
-  st_asewkt(st_transform(the_geom, 2154))
-from point;
